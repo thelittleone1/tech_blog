b936e8f20fe4373dcd1312a4dcdc0d8ab5aeaf4d
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-05-26 23:10:30 -0700

    fixed mistake,  put comment model in server.js

---
+++ models/Comment.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init (
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        comment_text: {
+            type: DataType.STRING,
+            allowNull: false,
+            validate: {
+                length:[1]
+            }
+        },
+        post_id: {
+            type: DataType.INTEGER,
+            allowNull: false,
+            references: {
+                model: "post",
+                key: "id",
+            }
+        }
+        
+    }
+)
