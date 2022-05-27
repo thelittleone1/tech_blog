af819a78a5bf1f4a1c433b2d573fef7eacc753d3
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-05-26 23:29:40 -0700

    working on user model

--- models/User.js
+++ models/User.js
@@ -2,4 +2,19 @@ const { Model, DataTypes } = require("sequelize");
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
-class User extends Model {}
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+      }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+    }
+)
