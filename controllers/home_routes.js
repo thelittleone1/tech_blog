ea26084ea6b771fd99595770336eefeae80c90cd
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-05-30 20:57:08 -0700

    working on home route

---
+++ controllers/home_routes.js
@@ -0,0 +1,9 @@
+// Constants for the routes that the user interacts directly with
+const router = require('express').Router();
+const sequelize = require("../config/connection");
+const { Comment, Post, User } = require("../models");
+
+// Once User logins, this route will direct them to the homepage
+router.get("/login", (req, res) => {
+    
+})
