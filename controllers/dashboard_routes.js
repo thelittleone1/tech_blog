2fa074215fc4b5045443e2dc4414cd860fd11498
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-05-30 22:32:34 -0700

    working on the edit route for my dashboard routes

--- controllers/dashboard_routes.js
+++ controllers/dashboard_routes.js
@@ -4,6 +4,7 @@ const sequelize = require("../config/connection");
 const { Comment, Post, User } = require("../models");
 const authorize = require("../utils/auth");
 
+// Route to edit posts
 router.get("/edit/:id", authorize, (req,res) => {
     Post.findOne({
         where: {
@@ -20,7 +21,30 @@ router.get("/edit/:id", authorize, (req,res) => {
             {
                 model: User,
                 attributes: ["user_name"],
+            },
+            {
+                model: Comment,
+                attributes: [
+                    "id",
+                    "comment_text",
+                    "post_id",
+                    "user_id",
+                ],
+                include: {
+                    model: User,
+                    attributes: ["user_name"]
+                }
             }
         ]
     })
-})
+    .then(userPostedData => {
+        // Serialize data so the template can read it
+        const postedData = userPostData.get({ plain: true });
+        // Pass serialized data and session flag into template
+        res.render("edit_posts", { postedData, logged_in: true});
+    })
+    .catch(err => {
+        console.log(err);
+        res.statusCode(500).json(err);
+    });
+});
