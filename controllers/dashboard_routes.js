0d347e56cb4f4c4577fd214be75d89e8fb705bb4
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-05-30 22:40:39 -0700

    added a route to render posts, used the other route to model it after

--- controllers/dashboard_routes.js
+++ controllers/dashboard_routes.js
@@ -4,6 +4,47 @@ const sequelize = require("../config/connection");
 const { Comment, Post, User } = require("../models");
 const authorize = require("../utils/auth");
 
+// Route to display posts
+router.get("/edit/:id", authorize, (req,res) => {
+    Post.findOne({
+        where: {
+            id: req.params.user_id
+        },
+        attributes: 
+        [
+            "id",
+            "post_text",
+            "title",
+            "user_id",
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    "id",
+                    "comment_text",
+                    "post_id",
+                    "user_id",
+                ],
+                include: {
+                    model: User,
+                    attributes: ["user_name"]
+                }
+            }
+        ]
+    })
+    .then(userPostData => {
+        // Serialize data so the template can read it
+        const postedData = userPostData.get({ plain: true });
+        // Pass serialized data and session flag into template
+        res.render("display_posts", { postedData, logged_in: true});
+    })
+    .catch(err => {
+        console.log(err);
+        res.statusCode(500).json(err);
+    });
+});
+
 // Route to edit posts
 router.get("/edit/:id", authorize, (req,res) => {
     Post.findOne({
@@ -37,7 +78,7 @@ router.get("/edit/:id", authorize, (req,res) => {
             }
         ]
     })
-    .then(userPostedData => {
+    .then(userPostData => {
         // Serialize data so the template can read it
         const postedData = userPostData.get({ plain: true });
         // Pass serialized data and session flag into template
