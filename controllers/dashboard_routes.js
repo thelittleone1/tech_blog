e3de277e43b855fc71b5a33541d7611208745318
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-05-30 22:27:21 -0700

    working on dashboard route with edit option

--- controllers/dashboard_routes.js
+++ controllers/dashboard_routes.js
@@ -4,3 +4,23 @@ const sequelize = require("../config/connection");
 const { Comment, Post, User } = require("../models");
 const authorize = require("../utils/auth");
 
+router.get("/edit/:id", authorize, (req,res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: 
+        [
+            "id",
+            "post_text",
+            "title",
+            "user_id",
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ["user_name"],
+            }
+        ]
+    })
+})
