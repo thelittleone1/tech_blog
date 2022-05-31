728e4349128228439066c57d2bff72f71c692a6c
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-05-30 21:26:25 -0700

    workong on post all route in homeroutes

--- controllers/home_routes.js
+++ controllers/home_routes.js
@@ -3,9 +3,38 @@ const router = require('express').Router();
 const sequelize = require("../config/connection");
 const { Comment, Post, User } = require("../models");
 
+// Rendering the homepage
+router.get("/", (res, req) => {
+    console.log(req.session);
+
+    Post.findAll({
+        attributes: [
+            "id",
+            "post_text",
+            "title",
+            "user_id",
+        ], 
+            include: {
+                model: Comment,
+                attributes: 
+                    [
+                        "id",
+                        "comment_text",
+                        "post_id",
+                        "user_id",
+                    ],
+                        include: {
+                            model: User,
+                            attributes: "user_name"
+                        }
+            }
+
+    })
+})
+
 // Once User logins, this route will direct them to the homepage
 router.get("/login", (req, res) => {
-    if(req.session.loggedIn) {
+    if(req.session.logged_in) {
         res.redirect("/");
         return;
     }
