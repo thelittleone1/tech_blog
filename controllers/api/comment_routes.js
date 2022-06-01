5d519f13ef6c5d96f71198caae6ce8b4cbb2fc25
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-05-31 22:34:58 -0700

    working on routes in comments routes

--- controllers/api/comment_routes.js
+++ controllers/api/comment_routes.js
@@ -22,7 +22,31 @@ router.post("/", authorize, (req, res) => {
             post_id: req.body.post_id,
             user_id: req.session.user_id,
         })
+        .then(userPostData => res.json(userPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
     }
 });
 
 // Route to delete comments
+router.delete("/:id", authorize, (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(userPostData => {
+        if (!userPostData) {
+            res.status(404).send("You got no comment with that id");
+            return;
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.statusCode(500).json(err);
+    });
+});
+
+module.exports = router;
