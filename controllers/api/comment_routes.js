1f7ae3ea45963a623db62f290f8cbdcfbc598c1d
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-05-31 22:27:54 -0700

    adding route to get all comments

--- controllers/api/comment_routes.js
+++ controllers/api/comment_routes.js
@@ -3,3 +3,13 @@ const router = require('express').Router();
 const sequelize = require("../config/connection");
 const { Comment, Post, User } = require("../models");
 const authorize = require("../utils/auth");
+
+// Route to get all comments
+router.get("/", (req, res) => {
+    Comment.findAll({})
+    .then(userPostData => res.json(userPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
