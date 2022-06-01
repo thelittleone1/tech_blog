8a95a3fa067a836af1efac42520ba44dff0b98e0
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-05-31 20:22:42 -0700

    editing route to look up one user by id

--- controllers/api/user_routes.js
+++ controllers/api/user_routes.js
@@ -19,6 +19,42 @@ router.get("/", (req,res) => {
 // Route to get one user
 router.get("/:id", (req,res) => {
     User.findOne({
-
+        attribute: { exclude: ["[password]"] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+        {
+        model: Post,
+        attributes: [
+            "id",
+            "post_text",
+            "title",
+            "user_id"
+            ]
+        },
+        {
+            model: Comment,
+            attributes: [
+                "id",
+                "comment_text",
+                "post_id"
+            ],
+            include: {
+                model: Post,
+                attributes: ["title"]
+            }
+        }
+    ]
+    })
+    .then(userPostData => {
+        if (!userPostData) {
+            res.status(404).send("Could not find a post");
+            return;
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.statusCode(500).json(err);
     });
 });
